7c96903f2b5d907938b076feff2194f4a53a3a02
SirXise <93085446+SirXise@users.noreply.github.com>
2022-06-22 17:31:21 +0800

    Update Histogram.java

--- solutions/Histogram.java
+++ solutions/Histogram.java
@@ -1,81 +1,77 @@
 import java.util.*;
-import java.lang.*;
 /**
  *
  */
-class Histogram {
+public class Histogram {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        int NumCases = input.nextInt();
-        input.nextLine();
-        for(int i=0;i<NumCases;i++){
-            int numOfData = input.nextInt();
-            int numOfBin = input.nextInt();
-            input.nextLine();
-            int[] strArr2 = new int[numOfData];
-            for(int k=0;k<numOfData;k++){
-                strArr2[k] = input.nextInt();
+
+        Scanner input_data = new Scanner(System.in);
+        int no_of_cases = input_data.nextInt();
+
+        int c = 0;
+        while (c < no_of_cases) {
+            int no_of_data = input_data.nextInt();
+            int no_of_bins = input_data.nextInt();
+
+            int[] datapoints = new int[no_of_data];
+
+            int b = 0;
+
+            while (b < no_of_data) {
+                datapoints[b] = input_data.nextInt();
+                b++;
             }
-            input.nextLine();
-            int[] newArray = new int[numOfBin + 1];;
-            intervals(strArr2, numOfBin, newArray);
-            Frequency(strArr2,numOfBin,newArray);
-        }
+            input_data.nextLine();
 
-    }
-    public static int max(int[] array){
-        int max = 0;
-        for(int i=0;i< array.length;i++){
-            if(array[i]>max){
-                max = array[i];
-            }
-        }
-        return max;
-    }
-    public static int min(int[] array){
-        int min = array[0];
-        for(int i=0;i< array.length; i++){
-            if(array[i]<min){
-                min = array[i];
-            }
-        }
-        return min;
-    }
+            int maximum = datapoints[0];
+            int minimum = datapoints[0];
 
-    public static int intervals(int[] N2, int numOfBin, int[] newArray){
+            for (int i = 0; i < datapoints.length; i++) {
 
-        int min = min(N2);
-        int max = max(N2);
-        int cutoff = (max - min) / numOfBin;
-        int interval = min;
-        for (int i = 0; i <= numOfBin; i++) {
-            newArray[i] = interval;
-            interval += cutoff;
-
-        }
-        String line = "";
-        for (int inter: newArray) {
-            line += inter + " ";
-        }
-        System.out.println(line);
-        return cutoff;
-    }
-
-    public static void Frequency(int[] N2, int numOfBin, int[] newArray){
-        int[] frequency = new int[numOfBin];
-        frequency[frequency.length-1] = 1;
-        for (int number: N2) {
-            for (int i = 0; i < newArray.length-1; i++) {
-                if (number >= newArray[i] && number<newArray[i+1]){
-                    frequency[i]++;
+                if (datapoints[i] > maximum) {
+                    maximum = datapoints[i];
+                } else if (datapoints[i] < minimum) {
+                    minimum = datapoints[i];
                 }
             }
-        }
-        String line1="";
-        for (int freq: frequency) {
 
-            line1 += freq + " ";
+            int cutoffs_value = (maximum - minimum) / no_of_bins;
+            int sum = 0;
+            int[] interval = new int[no_of_bins + 1];
+            int[] count = new int[no_of_bins];
+            String line = "";
+
+            for (int j = 0; j < interval.length; j++) {
+                if (j == 0) {
+                    sum += minimum;
+                    interval[j] = minimum;
+                    line += minimum + " ";
+                } else {
+                    interval[j] = sum;
+                    line += sum + " ";
+                }
+                sum += cutoffs_value;
+            }
+            System.out.println(line);
+
+            for (int k = 0; k < datapoints.length; k++) {
+                for (int l = 0; l < interval.length - 1; l++) {
+                    if (datapoints[k] >= interval[l] && datapoints[k] < interval[l + 1]) {
+                        count[l]++;
+                    }
+                }
+                if (datapoints[k] == interval[interval.length - 1]) {
+                    count[interval.length - 2]++;
+                }
+            }
+
+            String line1 = "";
+            for (int m = 0; m < count.length; m++) {
+                line1 += count[m] + " ";
+            }
+            System.out.println(line1);
+            c++;
         }
-        System.out.println(line1);
     }
+
 }
